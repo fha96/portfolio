ba8842a22eeca3e45021490a2d2c7712b8c0ee0d
fha96 <fahadalrimawe96@hotmail.com>
2022-07-30 16:52:40 +0300

    complete editing

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -63,7 +63,7 @@ class Experience extends Component {
     }
 
     return (
-      <section id="resume" className="pb-5">
+      <section id="resume" className="pb-5" >
         <div className="col-md-12 mx-auto">
           <div className="col-md-12">
             <h1 className="section-title" style={{ color: "black" }}>
